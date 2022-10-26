16d205fd40013a2807e108ccdaf8a9e0e8162bfe
Prateek Mishra <64471391+pra-cloud@users.noreply.github.com>
2022-10-26 18:10:21 +0530

    customfile

---
+++ grafana_push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+z=$(ps -aux)
+while read -r z
+do
+  var=$var$(awk '{print "'$host'_cpu_usage{process=\""$11"\", pid=\""$2"\"}", $3z}');
+done <<< $z
+curl -X POST -H  "Content-Type: text/plain" --data "$var" http://165.22.210.27:9091/metrics/job/top/instance/machine
+EOF
